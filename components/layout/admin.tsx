5d28c18053796fc9c94cbf2e4e82f50937542f90
Tran Vuong Duy <tranvuongduy2003@gmail.com>
2023-10-15 14:38:32 +0700

    Complete sidebar component

--- components/layout/admin.tsx
+++ components/layout/admin.tsx
@@ -1,12 +1,20 @@
-import { LayoutProps, Role } from '@/models';
+import { LayoutProps } from '@/models';
 import { useAuthStore } from '@/stores';
 import { useRouter } from 'next/router';
+import { Sidebar } from '../common';
 
 export function AdminLayout({ children }: LayoutProps) {
   const router = useRouter();
   const { profile } = useAuthStore();
 
-  if (!profile?.id || profile?.role !== Role.ADMIN) router.push('/auth/login');
+  // if (!profile?.id || profile?.role !== Role.ADMIN) router.push('/auth/login');
 
-  return <>{children}</>;
+  return (
+    <>
+      <div className="flex w-full">
+        <Sidebar />
+        <>{children}</>
+      </div>
+    </>
+  );
 }
---
+++ pages/user.tsx
@@ -0,0 +1,10 @@
+import { AdminLayout } from '@/components/layout';
+import { NextPageWithLayout } from '@/models';
+
+const UserPage: NextPageWithLayout = props => {
+  return <div>UserPage</div>;
+};
+
+UserPage.Layout = AdminLayout;
+
+export default UserPage;
