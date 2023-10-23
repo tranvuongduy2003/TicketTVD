ad991eb76151b20dab3d13285fe7a82ebfe906f5
Trần Vương Duy <61134564+tranvuongduy2003@users.noreply.github.com>
2023-10-23 21:45:32 +0700

    Change auth flow (#6)

--- apis/user.api.ts
+++ apis/user.api.ts
@@ -2,6 +2,8 @@ import httpRequest from '@/api-client/httpRequest';
 import { API_ROUTE } from '@/constants';
 import { User } from '@/models';
 
-export const getUsers = () => {
-  return httpRequest.get<User[]>(API_ROUTE.user);
+export const userApi = {
+  getUsers: () => {
+    return httpRequest.get<User[]>(API_ROUTE.user);
+  }
 };
---
+++ hooks/use-users.ts
@@ -0,0 +1,18 @@
+import { userApi } from '@/apis';
+import { QUERY_KEY } from '@/constants';
+import useSWR from 'swr';
+import { SWRConfiguration } from 'swr/_internal';
+
+export function useUsers(options?: Partial<SWRConfiguration>) {
+  const {
+    data: users,
+    error,
+    isLoading
+  } = useSWR(QUERY_KEY.users, () => userApi.getUsers(), {
+    revalidateOnMount: true,
+    keepPreviousData: true,
+    ...options
+  });
+
+  return { users, error, isLoading };
+}
